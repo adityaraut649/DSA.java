31a5bca789d1ae4fcfca9419056b8285bc827586
ADITYA RAUT <95859957+adityaraut649@users.noreply.github.com>
2025-09-17 10:56:24 +0530

    Add files via upload

---
+++ String_DSA/StringBasic.java
@@ -0,0 +1,64 @@
+package String;
+import java.util.*;
+public class StringBasic {
+    public static void main(String[] args) {
+
+        /* This is of normal way of String */
+
+        /* String str = "College Wallah";
+             System.out.println(str);*/
+
+        /*Scanner sc  =new Scanner(System.in);
+          String s = sc.next();
+          System.out.println(s);*/
+
+        /*Scanner sc  =new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);*/
+
+       /* String str = "College Wallah";
+        int len = str.length();
+        System.out.println(len);*/
+
+        //         Char
+
+      /*  String str = "College Wallah";
+        System.out.println(str.length());*/
+
+        /*String str = "College Wallah";
+        char ch = str.charAt(4);
+        System.out.println(ch);*/
+
+        /*String str = "College Wallah";
+        System.out.println(str.charAt(5));*/
+
+                // Index
+
+       /* String str = "Aditya";
+        System.out.println(str.indexOf('A'));*/
+
+        /*String str = "Ad";
+        String btr = "Om";
+        System.out.println(str.compareTo(btr));*/
+
+        /*String str = "Aditya";
+        System.out.println(str.contains("0"));*/     // output: false
+
+        /*String str = "Aditya";
+        System.out.println(str.startsWith("Ad"));*/
+
+        /*String str = "Aditya";
+        System.out.println(str.endsWith("Ad"));*/
+
+           //   concate
+       /* String str = "Aditya";
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());*/
+
+        /*String s1 = "Aditya";
+        String s2 = "Nisha";
+        String s3 = s1.concat(s2);
+        System.out.println(s3);*/
+
+    }
+}
