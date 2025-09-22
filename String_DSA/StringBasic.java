f5a5bc5c9d1203f689fca27d20d9e2c740d2c4da
ADITYA RAUT <95859957+adityaraut649@users.noreply.github.com>
2025-09-22 13:51:13 +0530

    Add files via upload

--- String_DSA/StringBasic.java
+++ String_DSA/StringBasic.java
@@ -1,64 +1,91 @@
 package String;
 import java.util.*;
+
 public class StringBasic {
     public static void main(String[] args) {
 
-        /* This is of normal way of String */
+        // ------------------------------------------
+        // Example 1: Normal string declaration
+        // ------------------------------------------
+//        String str = "College Wallah";
+//        System.out.println(str);
 
-        /* String str = "College Wallah";
-             System.out.println(str);*/
 
-        /*Scanner sc  =new Scanner(System.in);
-          String s = sc.next();
-          System.out.println(s);*/
+        // ------------------------------------------
+        // Example 2: Taking input
+        // ------------------------------------------
+//        Scanner sc = new Scanner(System.in);
+//        String s = sc.next();         // reads single word (stops at space)
+//        System.out.println(s);
 
-        /*Scanner sc  =new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println(s);*/
+//        Scanner sc = new Scanner(System.in);
+//        String s = sc.nextLine();     // reads entire line (with spaces)
+//        System.out.println(s);
 
-       /* String str = "College Wallah";
-        int len = str.length();
-        System.out.println(len);*/
 
-        //         Char
+        // ------------------------------------------
+        // Example 3: Length of a string
+        // ------------------------------------------
+//        String str = "College Wallah";
+//        int len = str.length();
+//        System.out.println(len);     // Output: 14
 
-      /*  String str = "College Wallah";
-        System.out.println(str.length());*/
 
-        /*String str = "College Wallah";
-        char ch = str.charAt(4);
-        System.out.println(ch);*/
+        // ------------------------------------------
+        // Example 4: charAt
+        // ------------------------------------------
+//        String str = "College Wallah";
+//        char ch = str.charAt(4);
+//        System.out.println(ch);      // Output: e
 
-        /*String str = "College Wallah";
-        System.out.println(str.charAt(5));*/
+//        System.out.println(str.charAt(5)); // Output: g
 
-                // Index
 
-       /* String str = "Aditya";
-        System.out.println(str.indexOf('A'));*/
+        // ------------------------------------------
+        // Example 5: indexOf
+        // ------------------------------------------
+//        String str = "Aditya";
+//        System.out.println(str.indexOf('A')); // Output: 0
 
-        /*String str = "Ad";
-        String btr = "Om";
-        System.out.println(str.compareTo(btr));*/
 
-        /*String str = "Aditya";
-        System.out.println(str.contains("0"));*/     // output: false
+        // ------------------------------------------
+        // Example 6: compareTo
+        // ------------------------------------------
+//        String str = "Ad";
+//        String btr = "Om";
+//        System.out.println(str.compareTo(btr));
+//        // Negative → str < btr, Positive → str > btr, 0 → equal
 
-        /*String str = "Aditya";
-        System.out.println(str.startsWith("Ad"));*/
 
-        /*String str = "Aditya";
-        System.out.println(str.endsWith("Ad"));*/
+        // ------------------------------------------
+        // Example 7: contains
+        // ------------------------------------------
+//        String str = "Aditya";
+//        System.out.println(str.contains("0"));  // false
 
-           //   concate
-       /* String str = "Aditya";
-        System.out.println(str.toLowerCase());
-        System.out.println(str.toUpperCase());*/
 
-        /*String s1 = "Aditya";
-        String s2 = "Nisha";
-        String s3 = s1.concat(s2);
-        System.out.println(s3);*/
+        // ------------------------------------------
+        // Example 8: startsWith / endsWith
+        // ------------------------------------------
+//        String str = "Aditya";
+//        System.out.println(str.startsWith("Ad")); // true
+//        System.out.println(str.endsWith("ya"));   // true
 
+
+        // ------------------------------------------
+        // Example 9: toLowerCase / toUpperCase
+        // ------------------------------------------
+//        String str = "Aditya";
+//        System.out.println(str.toLowerCase()); // aditya
+//        System.out.println(str.toUpperCase()); // ADITYA
+
+
+        // ------------------------------------------
+        // Example 10: concat
+        // ------------------------------------------
+//        String s1 = "Aditya";
+//        String s2 = "Nisha";
+//        String s3 = s1.concat(s2);
+//        System.out.println(s3);  // Output: AdityaNisha
     }
 }
